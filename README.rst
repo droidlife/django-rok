fba6c0bd12920526912b5fa26f67081607950393
Ankur Jain <ankurj630@gmail.com>
2018-07-17 20:04:53 +0530

    setup.py updated

---
+++ README.rst
@@ -0,0 +1,15 @@
+=====
+Django-Rok
+=====
+
+Polls is a simple Django app which provide public url for exposing your local web server.
+
+Quick start
+-----------
+
+1. Add "django_rok" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = [
+        ...
+        'django_rok',
+    ]
--- setup.py
+++ setup.py
@@ -1,9 +1,30 @@
 from setuptools import setup, find_packages
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django_rok',
-    version="0.1",
-    description='none',
+    name='django-rok',
+    version='0.1',
     packages=find_packages(),
-    license="MIT",
+    include_package_data=True,
+    license='MIT',
+    description='Public url for your local web server.',
+    long_description=README,
+    author='Ankur Jain',
+    author_email='ankurj630@gmail.com',
+    url='https://github.com/droidlife/django-rok',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
